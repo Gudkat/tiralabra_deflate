554d325e25dfc78c04bbf291d7ca2befc02686be
Timo Paloneva <timo.paloneva@gmail.com>
2025-09-12 14:32:18 +0300

    remove random data generation possibility, since random data testing is doesn't make sense

--- src/app.py
+++ src/app.py
@@ -18,53 +18,41 @@ Options:
                         available algorithms: 
                             huffman
                             lz77        [CURRENTLY MISSING]
-                            deflate     [CURRENTLY MISSING]
     -d                  Used to indigate to the program that we are decompressing the data. To compress the data, omit this flag.
-    -g <int>            Number indicates how much test data is generated. The number is character to the power of the given argument.
-                        For example command `poetry run python app.py -g 5` will generate 10**5 = 100 000 characters of test data in ASCII. This corresponds to little bit under = 100 kb of test data.
-                        
-                        Example: To save store the test data to file, use it like `poetry run python app.py -g 5 > test_data`
-          
 Example how to compress data using the algorithm: 
     `poetry run python app.py algorithm huffman test_data`
 """)
     exit()
 
 
-def main(filename: str, encode: bool, algo:str =None):
-    if not algo:
-        print("no regocnizable algorithm entered")
-    
+def main(filename: str, encode: bool, algo=None):
     with open(filename, "r") as file:
         data = file.read()
 
     if algo == 'huffman':
         chosen_alorithm = Huffman()
 
-    ret = chosen_alorithm.encode(data)
-    print(ret)
+    if not algo:
+        print("no regocnizable algorithm entered")
+        exit()
 
-def generate_test_data(magnitude_of_bytes):
-    random_letters = "".join(random.choices(ascii_letters, k=10**magnitude_of_bytes))
-    print(random_letters)
+    ret = chosen_alorithm.encode(data)
+    
+    print(ret)
 
 
 if __name__ == "__main__":
     implemented_algorithms = ['huffman']
-    # n = 1000000
     encode = True
+
     if "-h" in sys.argv or "help" in sys.argv:
         output_help()
-    if "-g" in sys.argv:
-        magnitude_of_bytes = int(sys.argv[sys.argv.index("-g") + 1])
-        generate_test_data(magnitude_of_bytes)
-        exit()
     if "-d" in sys.argv:
         encode = False
     if "algorithm" in sys.argv:
         algo = (sys.argv[sys.argv.index("algorithm") + 1])
         if algo not in implemented_algorithms:
-            print("Currently only the following compression algorithms are available")
+            print("Currently only the following compression algorithms are implemented")
             for algo in implemented_algorithms:
                 print(" - " + algo)
             exit()
