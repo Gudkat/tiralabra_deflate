5f8c4f14a96582354fe735a933cb7a934542fe22
Timo Paloneva <timo.paloneva@gmail.com>
2025-09-15 09:31:34 +0300

    nothing completed. computer dying. just saving progress

--- src/algorithms/huffman.py
+++ src/algorithms/huffman.py
@@ -1,6 +1,5 @@
 import heapq
 from collections import Counter
-
 from bitarray import bitarray
 
 
@@ -12,9 +11,17 @@ class Node:
         self.right = right
         self.name = node_name
 
+    # Comparision required for heap.
+    # If same freq, we go based on alphabets
+    # Might be better to make secondary ordering based on how many chars in name
+    # And tertiary comparision based on alphabets to create more balanced huffman tree
     def __lt__(self, other):
         return (self.value, self.name) < (other.value, other.name)
 
+    @property
+    def is_leaf(self):
+        return not(self.left or self.right)
+    
     def __str__(self):
         # for debugging. Delete later
         return f"name: {self.name}, value: {self.value}"
@@ -25,17 +32,21 @@ class Huffman:
     Encodes huffman coding for iterable input. encode method accepts iterables and returns a tuple; first item encoded bits and 2nd item encoding "table"
     TODO: 
     * Include the codes for the binary encodings
-    * Something needs to be done for LZ77 backreference encoding
     * decode method missing
     '''
 
     def __add_nodes(self, nodes):
         node_counts = Counter(nodes)
+
+        # Check if it's better to 
+        # as_nodes = [Node(value, name) for value,name in node_count.items()]
+        # self.__heap = heapq.heapify(as_nodes)
+
         for name, value in node_counts.items():
             heapq.heappush(self.__heap, Node(value, name))
 
     def __create_tree(self):
-        while True:
+        while len(self.__heap) != 1:
             right, left = heapq.heappop(self.__heap), heapq.heappop(self.__heap)
             new_node = Node(
                 value=left.value + right.value,
@@ -48,13 +59,13 @@ class Huffman:
             if not self.__heap:
                 return new_node
 
-            # otherwise add the new node back to heap
+            # otherwise add the parent node back to heap
             heapq.heappush(self.__heap, new_node)
 
     @classmethod
-    def __create_codes(cls, node, codes):
+    def __create_codes(cls, node, codes:dict):
 
-        if not node.left and not node.right:
+        if node.is_leaf:
             codes[node.name] = bitarray(node.bits)
             return
 
@@ -67,26 +78,42 @@ class Huffman:
     def encode(self, iterable):
         self.__heap = []
         codes = {}
+        encoded_tree = bitarray()
+        encoded_content = bitarray()
 
         self.__add_nodes(iterable)
         root = self.__create_tree()
         self.__create_codes(root, codes)
 
-        encoded_bits = bitarray()
-        for sym in iterable:
-            encoded_bits.extend(codes[sym])
+        self.__encodeNode(root, encoded_tree)
 
-        # Figure out how to include the codes in the encoded part
-        return encoded_bits, codes
+        for symbol in iterable:
+            encoded_content.extend(codes[symbol])
+
+
+        print(encoded_tree)
+        return encoded_tree.extend(encoded_content)
 
     def decode(self, encodedbits):
         raise NotImplementedError
 
+    def __encodeNode(self, node:Node, encoded_tree):
+        # Encoding huffman tree is based on 
+        # https://stackoverflow.com/questions/759707/efficient-way-of-storing-huffman-tree
+        print(encoded_tree)
+
+        if node.is_leaf:
+            encoded_tree.extend('1')
+            encoded_tree.extend(format(b, '08b') for b in node.name.encode('utf-8'))
+        else:
+            encoded_tree.extend('0')
+            self.__encodeNode(node.left, encoded_tree)
+            self.__encodeNode(node.right, encoded_tree)
+
 
 if __name__ == "__main__":
-    test_string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    huffman = Huffman()
-    encoded_bits, codes = huffman.encode(test_string)
+    test_string = "A"
 
-    print(encoded_bits)
-    print(codes)
+    huffman = Huffman()
+    encoded_bin = huffman.encode(test_string)
+
